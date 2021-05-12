3e3821a09ed4ff2077dec0cb347634b7e5c3adc6
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-12 20:34:25 +0200

    Product.cs file improved

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -3,8 +3,10 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
-        public Product()
-        {
-        }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
     }
 }
