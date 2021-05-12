7710ad06a8deac74d14ea434d4c451bef7faf96f
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-12 20:52:42 +0200

    HomeController.cs file improved with examples

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
 using LanguageFeatures.Models;
+using System.Collections.Generic;
 
 namespace LanguageFeatures.Controllers
 {
@@ -24,6 +25,36 @@ namespace LanguageFeatures.Controllers
 
             //Generating view
             return View("result", (object)String.Format("Product name: {0}", productName));
+        }
+
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product
+            {
+                //setting value property with object initializR
+                ProductID = 100,
+                Name = "MacBook Air",
+                Description = "One of the cutest ultrabooks",
+                Price = 7699,
+                Category = "Ultrabooks"
+            };
+
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        //Example of using dictionaries with collections, it is much easier and comfortable to use
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "MacBook Air", "MacBook Pro", "Mac Mini", "iMac" };
+
+            List<int> intList = new List<int> { 10, 20, 30, 40, 50 };
+
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                {"MacBook Air", 10}, {"MacBook Pro", 20}, {"Mac Mini", 30}, {"iMac", 40}
+            };
+
+            return View("Result", (object)stringArray[1]);
         }
     }
 }
