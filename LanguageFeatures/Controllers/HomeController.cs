2beaec65c240c65c68fa4268e3e1ffcaf7c3d323
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-12 20:26:19 +0200

    Improved HomeController.cs file

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View ();
+            return "Passage to URL address displaying example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            //creating new object Product
+            Product myProduct = new Product();
+
+            //setting value of property
+            myProduct.Name = "MacBook Pro";
+
+            //Reading property
+            string productName = myProduct.Name;
+
+            //Generating view
+            return View("result", (object)String.Format("Product name: {0}", productName));
         }
     }
 }
